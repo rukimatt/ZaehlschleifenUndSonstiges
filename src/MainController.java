41e7729908b8a1cd941dccab901c8d581e98e790
rukimatt <rukimatt@kkg-do.eu>
2023-04-25 10:26:48 +0200

    Changes

--- src/MainController.java
+++ src/MainController.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class MainController {
+public class MainController {
+
+    public static void main(String[] args) {
+    }
+
+    public static void printSomeNumbers() {
+        for (int i = 6; i < 24; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void printNumbers(int start, int end) {
+        for (int i = start; i <= end; i++) {
+            System.out.println(i);
+        }
+
+    }
+
+    public static int sumUp(int start, int end) {
+        int sum = 0;
+        for (int i = start; i <= end; i++) {
+            sum = sum + i;
+        }
+        return sum;
+    }
 }
