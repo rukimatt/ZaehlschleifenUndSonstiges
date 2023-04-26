c884f99c7e8485b04685bc8ffe5f0946ae4072f6
rukimatt <rukimatt@kkg-do.eu>
2023-04-26 14:52:24 +0200

    Changes

--- src/MainController.java
+++ src/MainController.java
@@ -1,10 +1,13 @@
 public class MainController {
 
     public static void main(String[] args) {
-       // System.out.println(powerOfTwo(5));
-       // System.out.println(sumUpPowerOfTwo(8));
-       // rectangleStars(5, 4);
-        triangleStars(9);
+        // System.out.println(powerOfTwo(5));
+        // System.out.println(sumUpPowerOfTwo(8));
+        // rectangleStars(5, 4);
+        // triangleStars(9);
+        // printEvenNumbers(0, 10);
+        // System.out.println(sumUpThree(0, 10));
+        starThing(5, 8);
     }
 
     //Aufgabe 2, Einfache Methoden
@@ -23,21 +26,23 @@ public class MainController {
 
     public static int sumUp(int start, int end) {
         int sum = 0;
-        for(int i = start; i <= end; i++) {
+        for (int i = start; i <= end; i++) {
             sum = sum + i;
         }
         return sum;
     }
-    public static int powerOfTwo(int p){
-        if(p > 0) return -1;
 
-        int result  = 1;
-        for(int i = 0; i < p; i++){
+    public static int powerOfTwo(int p) {
+        if (p > 0) return -1;
+
+        int result = 1;
+        for (int i = 0; i < p; i++) {
             result = result * 2;
         }
-        return  result;
+        return result;
 
     }
+
     public static int sumUpPowerOfTwo(int n) {
         int sum = 0;
         for (int i = 0; i <= n; i++) {
@@ -45,27 +50,64 @@ public class MainController {
         }
         return sum;
     }
+
     //Aufgabe 3, Komplexere Anwendungen
     public static void rectangleStars(int width, int height) {
-        for(int i = 0; i < height; i++){
-            for(int j = 0; j <= width; j++){
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j <= width; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
 
     }
-    public static void rectangleStarsSave(int width, int height){
-        if(width> 0 && height > 0){
+
+    public static void rectangleStarsSave(int width, int height) {
+        if (width > 0 && height > 0) {
             rectangleStars(width, height);
         }
 
     }
-    public static void triangleStars(int width){
-        for(int i = 1; i <= width; i++){
+
+    public static void triangleStars(int width) {
+        for (int i = 1; i <= width; i++) {
             for (int j = 0; j < i; j++) System.out.print("*");
             System.out.println();
         }
 
+    }
+
+    public static void printEvenNumbers(int start, int end) {
+        for (int i = start; i <= end; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+
+        }
+    }
+
+    public static int sumUpThree(int start, int end) {
+        int sum = 0;
+        for (int i = start; i <= end; i++) {
+            if (i % 3 == 0) {
+                sum = sum + i;
+            }
+        }
+        return sum;
+
+    }
+
+    public static void starThing(int width, int height) {
+        for (int i = 0; i < height; i++) {
+            if (i % 2 == 1) {
+                System.out.print("*");
+            }else{
+                for (int j = 0; j < width; j++) {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+
+        }
     }
 }
